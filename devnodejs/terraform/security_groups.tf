22b5a235949c8f0ffb3b49365f64520596d97096
Peter Magliocco <peter.magliocco@bsigroup.com>
2026-05-15 11:50:16 +0100

    upated security group

---
+++ devnodejs/terraform/security_groups.tf
@@ -0,0 +1,32 @@
+
+resource "aws_security_group" "ci_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["your-ip/32"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
